d499b7682d795345fa24e82ef568b6c82ba53768
SujanPrasadPandey <88sujanpandey@gmail.com>
2026-04-12 13:45:42 +0545

    cloudflare url and stuff

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'), 
+        'http://127.0.0.1:3000',
+        'https://dashboard.kioch.org.np'
+    ],
 
     'allowed_origins_patterns' => [],
 
